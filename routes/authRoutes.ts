39f0b6aabaae5c82dcff0f10465aabd9f01b7929
oswi <oswaldo.adrian@live.com.mx>
2022-05-06 19:21:17 -0500

    renew route

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
   createUser,
-  loginUser
+  loginUser,
+  renewToken
 } from '../controllers/authController';
 
 const router = Router();
 
 router.post("/", loginUser);
 router.post("/create", createUser);
+router.get("/renew", renewToken);
 
 export default router;
